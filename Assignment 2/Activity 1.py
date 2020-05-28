8c496bbade5014da8a265731b3f926fa38540ef4
morrisignacio <65792095+morrisignacio@users.noreply.github.com>
2020-05-28 12:46:41 -0400

    Update Activity 1.py

--- Assignment 2/Activity 1.py
+++ Assignment 2/Activity 1.py
@@ -1,2 +1,3 @@
+# This program displays Hello name, you can change and display any name you desire by just changing the string name and what name is going to be = to.
 name = "Morris"
 print("Hello " + name + " !")
